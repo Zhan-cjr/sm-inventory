34771ce8e3ed19578f501c9d665a14848f1cf0ef
amnalzhan <amnalzhan@gmail.com>
2026-06-24 19:46:57 +0700

    fix: N+1 python api calls causing cloudflare timeout

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -23,7 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(\App\Services\SuggestedOrderService::class, function ($app) {
+            return new \App\Services\SuggestedOrderService();
+        });
     }
 
     /**
