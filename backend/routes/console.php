e26308b6ff943b8272fa75963b7e79decfce3309
amnalzhan <amnalzhan@gmail.com>
2026-06-24 20:28:13 +0700

    fix: N+1 python api calls causing cloudflare timeout

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+
+// Menjalankan pengecekan produk discontinue setiap jam 12 malam
+Schedule::command('inventory:auto-discontinue')->dailyAt('00:00');
+
+// Menjalankan pengecekan dan pembuatan draft promo otomatis setiap jam 1 dini hari
+Schedule::command('inventory:auto-pricing')->dailyAt('01:00');
